146070497323c6f8e85cf7c1f233321bea43b20b
Nick648 <dimitriy648@yandex.ru>
2023-01-27 13:32:20 +0300

    update

--- Text_from_photo.py
+++ Text_from_photo.py
@@ -1,5 +1,7 @@
 import time
 import os
+import keyboard
+import pyperclip
 from PIL import ImageGrab
 import requests
 
@@ -19,6 +21,44 @@ RED = Fore.LIGHTRED_EX
 YELLOW = Fore.LIGHTYELLOW_EX
 RESET = Style.RESET_ALL
 
+# Reading the clipboard
+def reading_clipboard():
+    print(YELLOW + "=" * 40 + RESET)
+    pyperclip.copy('Easter Egg')  # If there is already a screen at startup, do not take it
+    old_copy = None
+    count = 0
+    while True:
+        try:
+            copy_im = ImageGrab.grabclipboard()
+        except Exception as ex:  # OSError: failed to open clipboard
+            copy_im = None
+            print(RED + f'Reading error! Check that the screenshot is saved correctly.  Error: {ex}' + RESET)
+
+        if old_copy != copy_im and copy_im is not None:
+            count += 1
+            old_copy = copy_im
+            # time_now = datetime.datetime.today()
+            # time_h, time_m, time_s = time_now.hour, time_now.minute, time_now.second
+            # name_screen = os.path.join(WAY_DIR, f'Screen {count}  ({time_h}-{time_m}-{time_s}).png')
+            print(copy_im, type(copy_im))
+            copy_im.save('screen_pic_del.png', 'PNG')
+            test_files = {"test_file_1": open('screen_pic_del.png', "rb")}
+
+            r = requests.post('https://img2txt.com/ru/file/upload-dropdown', files=test_files)
+            print(r)
+            print(r.content)
+            print(r.json())
+
+            test_files["test_file_1"].close()
+            time.sleep(1)
+            os.remove('screen_pic_del.png')
+            # print(f'\t{GREEN + f"Copy {count} Done!" + RESET} Save as: {name_screen}')
+            # copy_im.save(name_screen, 'PNG')
+            print(YELLOW + "=" * 40 + RESET)
+        if keyboard.is_pressed('Esc'):
+            break
+
+        time.sleep(1)  # The problem with ImageGrab.grabclipboard() if you remove sleep
 
 def main():
     print(YELLOW + "=" * 30 + RESET)
@@ -30,11 +70,6 @@ def main():
             old_copy = copy_im
             copy_im.save('screen_pic_del.png', 'PNG')
 
-            test_files = {"test_file_1": open('screen_pic_del.png', "rb")}
-            r = requests.post('https://img2txt.com/ru', files=test_files)
-            test_files["test_file_1"].close()
-
-            print(r, r.text, sep='\n')
             print(YELLOW + "=" * 30 + RESET)
             time.sleep(1)
             os.remove('screen_pic_del.png')
@@ -42,5 +77,16 @@ def main():
         time.sleep(1)
 
 
+def test():
+    test_files = {"test_file_1": open('screen_pic_del.png', "rb")}
+    r = requests.post('https://httpbin.org/post', files=test_files)
+    print(r)
+    print(r.content)
+    print(r.json())
+    test_files["test_file_1"].close()
+
+
 if __name__ == '__main__':
-    main()
+    # test()
+    # main()
+    reading_clipboard()
